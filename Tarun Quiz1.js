cf2e576515abd3da73f85c44c542bb9576e470d4
Tarun Kumar Dey <111647518+Tarun123a@users.noreply.github.com>
2022-12-07 15:57:29 +0530

    Add files via upload

---
+++ Tarun Quiz1.js
@@ -0,0 +1,115 @@
+// add questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "HTML stands for -",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: " HTML files are saved by default with the extension?",
+    answer: ".html",
+    options: [
+      ".h",
+      ".http",
+      ".html",
+      "None of the above"
+    ]
+  },
+      {
+    numb: 7,
+    question: "What are the attributes used to change the size of an image?",
+    answer: "width and height",
+    options: [
+      "width and height",
+      "Big and Small",
+      "Top and bottom",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Javascript is an _______ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: " How can a datatype be declared to be a constant type?",
+    answer: "const",
+    options: [
+      "var",
+      "const",
+      "let",
+      "constant"
+    ]
+  },
+  
+];
