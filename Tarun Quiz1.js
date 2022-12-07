f5d7d1ba4fec6a0ad0992ffeef6c50ab767f19f2
Tarun Kumar Dey <111647518+Tarun123a@users.noreply.github.com>
2022-12-07 17:28:46 +0530

    Add files via upload

---
+++ Tarun Quiz1.js
@@ -0,0 +1,115 @@
+// add questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "HTML stands for -",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How can we change the background color of an element?",
+    answer: "background-color",
+    options: [
+      "color",
+      "background-color",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following are parts of the CSS box model?",
+    answer: "All of the above",
+    options: [
+      "Margins",
+      "Borders",
+      "Padding",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: " We can make rounded borders around elements using which CSS element?",
+    answer: "border-radius",
+    options: [
+      "border-collapse",
+      "border-radius",
+      "border-round",
+      "None of the above"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: " HTML files are saved by default with the extension?",
+    answer: ".html",
+    options: [
+      ".h",
+      ".http",
+      ".html",
+      "None of the above"
+    ]
+  },
+      {
+    numb: 7,
+    question: "What are the attributes used to change the size of an image?",
+    answer: "width and height",
+    options: [
+      "width and height",
+      "Big and Small",
+      "Top and bottom",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Javascript is an _______ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: " How can a datatype be declared to be a constant type?",
+    answer: "const",
+    options: [
+      "var",
+      "const",
+      "let",
+      "constant"
+    ]
+  },
+  
+];
